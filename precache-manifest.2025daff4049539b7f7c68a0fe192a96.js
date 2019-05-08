54f77d014717a61e9ae1d1aa3c002b658b168337
nick.dave.turner <37630270+nick-dave-turner@users.noreply.github.com>
2019-05-08 08:43:10 +0100

    Updates

---
+++ precache-manifest.2025daff4049539b7f7c68a0fe192a96.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1105a972037b2d0d9a211d70ba6f0974",
+    "url": "/stock-trade-app/index.html"
+  },
+  {
+    "revision": "068dfd4a3945edbb6b11",
+    "url": "/stock-trade-app/static/js/2.141607ef.chunk.js"
+  },
+  {
+    "revision": "7ae0514835888ec95b98",
+    "url": "/stock-trade-app/static/js/main.6d15dbc7.chunk.js"
+  },
+  {
+    "revision": "33e954d4e6825ccf16f5",
+    "url": "/stock-trade-app/static/js/runtime~main.5487add5.js"
+  }
+]);
